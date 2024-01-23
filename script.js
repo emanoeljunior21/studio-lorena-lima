6149c4a1c58c18681193b0205d0a88fc1c36b835
emanoeljunior21 <emanoeljuniordev@gmail.com>
2024-01-22 22:09:06 -0300

    Lorena Lima Studio & Academy

---
+++ script.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const footer = document.querySelector("footer");
+
+    footer.addEventListener("mouseover", function() {
+      
+        footer.style.backgroundColor = "#555";
+    });
+
+   
+    footer.addEventListener("mouseout", function() {
+      
+        footer.style.backgroundColor = "#333";
+    });
+});
+
+document.getElementById("instagramLink").addEventListener("click", function(event) {
+    event.preventDefault(); 
+
+    window.open(this.getAttribute("href"), "_blank");
+});
+
+document.getElementById("whatsappLink").addEventListener("click", function(event) {
+   
+    window.open(this.getAttribute("href"), "_blank");
+});
+
+document.getElementById("locationLink").addEventListener("click", function(event) {
+  
+    window.open(this.getAttribute("href"), "_blank");
+});
