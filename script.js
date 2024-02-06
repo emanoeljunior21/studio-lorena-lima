ff45967437b613a391525810821422c0e37b3c53
emanoeljunior21 <emanoeljuniordev@gmail.com>
2024-02-05 23:22:17 -0300

    alteração js1

--- script.js
+++ script.js
@@ -6,10 +6,9 @@
         dropdown.style.display = (dropdown.style.display === 'block') ? 'none' : 'block';
     }
 
-    // Add event listeners to each item with a dropdown
     document.querySelectorAll('.item').forEach(function(item) {
         item.addEventListener('click', function() {
-            // Toggle the dropdown when an item is clicked
+           
             toggleDropdown(item);
         });
     });
